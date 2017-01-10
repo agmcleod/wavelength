5e5defbab3a54d5019b9d14645608070294da5a4
Aaron McLeod <aaron.g.mcleod@gmail.com>
2017-01-10 00:07:00 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <div className="app-header">
+          <h2>Wavelength - audio converter</h2>
+        </div>
+        <p className="app-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
