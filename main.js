7ae828aae25bc9797e78215f0a2fa0d8bb3efb5a
Aaron McLeod <aaron.g.mcleod@gmail.com>
2017-05-05 11:27:52 -0400

    nice animation for fade in

--- main.js
+++ main.js
@@ -35,6 +35,8 @@ function createWindow () {
 
   win = new BrowserWindow({width: 800, height: 600});
 
+  // win.webContents.openDevTools();
+
   // and load the index.html of the app.
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'build', 'index.html'),
--- src/components/flash/index.js
+++ src/components/flash/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 import { connect } from 'react-redux';
 import { hideMessage } from '../../reducers/flash_message';
 import styles from './index.css';
@@ -6,10 +7,12 @@ import { TYPES } from '../../reducers/flash_message';
 
 export function Flash (props) {
   return (
-    <p className={props.type === TYPES.ERROR ? styles.error : styles.success}>
-      {props.message}
-      <button className={styles.close} onClick={props.hideMessage}>x</button>
-    </p>
+    <div className={classNames(props.type === TYPES.ERROR ? styles.error : styles.success, 'fadeIn')}>
+      <p className={styles.message}>{props.message}</p>
+      <button className={styles.close} onClick={props.hideMessage}>
+        <span>x</span>
+      </button>
+    </div>
   );
 }
 
